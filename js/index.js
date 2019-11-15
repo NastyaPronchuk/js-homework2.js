98461bf85e4e85ddfc5283906d434360b98df6d5
NastyaPronchuk <girloneshot@icloud.com>
2019-11-15 17:54:24 +0000

    Simple code

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// const result = { asd: 123};
+// let obj = {
+//     name: 'fjhj',
+//     add = () => {}
+// }
+
+// string
+// number
+// NaN
+// undefine
+// object
+// symbol
+// null
+// boolean
+//et result = 0;
+let result = document.getElementById("result");
+let operations = document.getElementById("operations");
+const array = ["+","-","%","/","*"];
+
+function addInfo(param){
+
+if(!operations.value && param === '0') return;
+
+
+
+if(!operations.value && !(+param)){
+    operations.value += `0${param}`;
+    return;
+}
+
+const length = operations.value.length;
+for (let index = 0, lastLetter = operations.value.charAt(length -1); index < array.length; index++) {
+  if (lastLetter === array[index]) {
+      operations.value = operations.value.slice(0, length-1) + param;
+     return;
+  };
+}
+
+operations.value += param;
+}
+function showResult(){
+    result.value = eval(operations.value);
+}
+
