fbf69c4fae425ba4c67ee0e361c9957365e3964b
NastyaPronchuk <girloneshot@icloud.com>
2019-11-15 18:22:43 +0000

    Simple code

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-// const result = { asd: 123};
-// let obj = {
-//     name: 'fjhj',
-//     add = () => {}
-// }
-
-// string
-// number
-// NaN
-// undefine
-// object
-// symbol
-// null
-// boolean
-//et result = 0;
 let result = document.getElementById("result");
 let operations = document.getElementById("operations");
 const array = ["+","-","%","/","*"];
@@ -31,9 +16,6 @@ if(!operations.value && !(+param)){
 const length = operations.value.length;
 for (let index = 0, lastLetter = operations.value.charAt(length -1); index < array.length; index++) {
   if (lastLetter === array[index] && array[index] === param) {
-      console.log('1', lastLetter);
-      console.log('1', array[index]);
-      console.log('1', lastLetter === array[index]);
       operations.value = operations.value.slice(0, length-1) + param;
      return;
   };
@@ -47,4 +29,5 @@ function showResult(){
 
 function reset(){
     operations.value = "";
+    result.value = "";
 }
